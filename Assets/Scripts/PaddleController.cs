0e21cec667c0c681c619c3ffc6ba56507562afba
leehs730 <leehs730@naver.com>
2023-12-01 15:31:35 +0900

    [Fix]paddle update
    패들 업데이트

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
-    public float speed = 2.5f;
+    [SerializeField] private float speed = 2.5f;
 
     void Start()
     {
@@ -14,8 +14,12 @@ public class PaddleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
+        OnMove();
+    }
 
+    private void OnMove()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(x, 0) * speed * Time.deltaTime;
     }
 }
