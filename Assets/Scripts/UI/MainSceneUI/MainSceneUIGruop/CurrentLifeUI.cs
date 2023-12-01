833229ec6be3de52e2647399dfb10928f7e31771
dolgang <149345680+dolgang@users.noreply.github.com>
2023-12-01 12:29:02 +0900

    [feature][1201] 라이프 이미지 토글 기능 추가
    - 현재 라이프 값에 따라 라이프 이미지를 토글하는 기능 구현 (최대 10)

--- Assets/Scripts/UI/MainSceneUI/MainSceneUIGruop/CurrentLifeUI.cs
+++ Assets/Scripts/UI/MainSceneUI/MainSceneUIGruop/CurrentLifeUI.cs
@@ -5,5 +5,60 @@ using UnityEngine;
 public class CurrentLifeUI : MonoBehaviour
 {
     private GameObject _currentLife;
+    private int _life;
 
+    public int CurrentLifeChange
+    {
+        get { return _life; }
+        set
+        {
+            if (_life != value)
+            {
+                LifeImageToggle();
+                
+            }
+
+            if (value < 10)
+            {
+                _life = value;
+            }
+            else
+            {
+                _life = 10;
+                Debug.Log("최대 라이프는 10을 초과할 수 없습니다");
+            }
+
+            
+        }
+    }
+
+
+    private void Awake()
+    {
+        _currentLife = gameObject;
+        CurrentLifeChange += 2;
+        LifeImageToggle();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void LifeImageToggle()
+    {
+        for (int i = 1; i <= _life; i++)
+        {
+            _currentLife.transform.Find("LifeImage" + i.ToString()).gameObject.SetActive(true);
+        }
+        for (int i = _life + 1; i <= 10; i++)
+        {
+            _currentLife.transform.Find("LifeImage" + i.ToString()).gameObject.SetActive(false);
+        }
+    }
 }
