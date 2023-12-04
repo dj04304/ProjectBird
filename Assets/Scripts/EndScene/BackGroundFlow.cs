021b6677e0f82883400d96c5a5e4cc67828bada7
Lawrence1031h <lawrence1031h@gmail.com>
2023-12-04 15:43:15 +0900

    20231204_중간저장
    End Scene 에서 BackGround 이미지 Flow 효과 추가,
    BackGround에 있던 Bird를 UI로 옮김

---
+++ Assets/Scripts/EndScene/BackGroundFlow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundFlow : MonoBehaviour
+{
+    public float scrollSpeed = 2f;
+
+    private void Update()
+    {
+        float moveX = Time.deltaTime * scrollSpeed;
+        transform.Translate(new Vector2(-moveX, 0));
+
+        if (transform.position.x < -10f)
+            transform.position = new Vector2(0, 0);
+    }
+
+}
