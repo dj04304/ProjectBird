93d7950ef35a272e4921a8636c14e666d9e1ee02
dolgang <149345680+dolgang@users.noreply.github.com>
2023-12-05 15:33:21 +0900

    [fix][1205] 현재 점수 UI 디버그 로직 추가
    - 디버그 오류 출력하는 로직 추가

--- Assets/Scripts/UI/MainSceneUI/MainSceneUIGruop/CurrentScoreText.cs
+++ Assets/Scripts/UI/MainSceneUI/MainSceneUIGruop/CurrentScoreText.cs
@@ -16,9 +16,19 @@ public class CurrentScoreText : MonoBehaviour
     public int ChangeCurrentScore
     {
         get { return _currentScore; }
-        set { 
+        set
+        {
             _currentScore = value;
-            _scoreText.text = "현재 점수\n" + _currentScore.ToString();
+
+            // 여기서 _scoreText가 null인지 체크
+            if (_scoreText != null)
+            {
+                _scoreText.text = "현재 점수\n" + _currentScore.ToString();
+            }
+            else
+            {
+                Debug.LogError("Score text is null. Make sure it is assigned in the inspector.");
+            }
         }
     }
 }
